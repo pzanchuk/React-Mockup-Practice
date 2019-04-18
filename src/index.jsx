cec93061c04f227e7c7b0898a6e7ca107b8a4c11
Student <example@example.com>
2019-04-18 15:16:42 -0700

    nested components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-ReactDOM.render(,
+import App from "./components/App";
+import Header from "./components/Header";
+
+ReactDOM.render(
+  <App/>,
   document.getElementById('react-app-root')
 );
