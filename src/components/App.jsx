a4cbd3c3d51783c9bd78c236a460f1db071603b1
Student <example@example.com>
2019-04-18 14:33:53 -0700

    add stuff

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return (  <div>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase will not save record!</em></p>
+    </div>);
+}
+
+export default App;
